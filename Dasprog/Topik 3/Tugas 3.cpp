56bde4f043bb5421262e2536bfd00c27e15569ee
Tacia Prayitno <taciaprayitno9@gmail.com>
2026-05-13 12:00:42 +0700

    update

--- Dasprog/Topik 3/Tugas 3.cpp
+++ Dasprog/Topik 3/Tugas 3.cpp
@@ -14,43 +14,30 @@ int main (){
 	printf("         ===========================\n");
 	printf("         Ready Praktikum Topik 3 !!!\n");
 	printf("         ===========================\n");
-	
 
 	printf("%-65s:","Masukkan Nama Lengkap");
 	gets (nama);
-	
 	printf("%-65s:","Masukkan Nama barang");
 	gets (barang);
-	
 	printf("%-65s:","Masukkan tanggal Hari ini (dd-mm-yyyy)");
-	scanf ("%[^-]-%[^-]-%s", dd, mm, yyyy);
-	
+	scanf ("%s-%s-%s", dd, mm, yyyy);
 	printf("%-65s:","Masukkan angka ribuan (R)");
 	scanf("%f", &R);
-	
 	printf("%-65s:","Masukkan 3 angka yang dipisahkan dengan tanda Underscore (a_b_c)");
 	scanf("%i_%i_%i", &a, &b, &c);
 	
-	system("cls");
+	system ("cls");
 	
-	int tambah= a + b + c;
-	int kali= a * b * c;
-	float mod= a % b % c;
-	float kalir= R * tambah;
-	float q= (float)a / b;
-	float r= (float)b / c;
-	float s= (float)a / c;
+	printf("Hello User %s!!!\n", nama);
+	printf("Date of Today: %s\n\n", dd, mm, yyyy);
+	printf("Hasil penjumlahan ABC   = %i\n", a + b + c);
+	printf("Hasil R dikali (a+b+c)%% = %.2f\n\n", (R * (a + b + c))/100);
 	
-	printf("Halo User %s!!\n", nama);
-	printf("Date of today: %s-%s-%s\n\n", dd, mm, yyyy);
+	float q= (float) a/b;
+	float r= (float) b/c;
+	float s= (float) a/c;
 	
-	printf("Hasil penjumlahan ABC   = %i\n", tambah);
-	printf("Hasil R dikali (a+b+c)% = %.2f\n\n", kalir/100);
-	
-	printf("Hasil Perkalian ABC     = %i\n", kali);
+	printf("Hasil Perkalian ABC     = %i\n", a * b * c);
 	printf("Hasil A dibagi B, B dibagi C, dan A dibagi C   = %.2f, %.2f, %.2f\n", q, r, s);
 	printf("Hasil A mod B, B mod C, A mod C                = %i, %i, %i", a % b, b % c, a % c);
 }
-	
-
-
